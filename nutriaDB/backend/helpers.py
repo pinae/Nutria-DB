75b663e043c2bd167fed8d3189e3110bbf9aa316
Pina Merkert <pina@pinae.net>
2018-12-08 11:04:12 +0100

    Make sure the helper does not crash if the name is not splittable

--- nutriaDB/backend/helpers.py
+++ nutriaDB/backend/helpers.py
@@ -3,9 +3,12 @@ import re
 
 
 def split_name(name_str):
-    mao = re.match(
-        "^\s*([\w \-\(\[\{\}\]\)\#\%\!\.\,\;\*]+)\s*:\s*([\w \-\(\[\{\}\]\)\#\%\!\.\,\;\*]+)\s*$",
-        name_str)
-    category_str = mao.groups()[0]
-    name_addition = mao.groups()[1]
-    return category_str, name_addition
+    try:
+        mao = re.match(
+            "^\s*([\w \-\(\[\{\}\]\)\#\%\!\.\,\;\*]+)\s*:\s*([\w \-\(\[\{\}\]\)\#\%\!\.\,\;\*]+)\s*$",
+            name_str)
+        category_str = mao.groups()[0]
+        name_addition = mao.groups()[1]
+        return category_str, name_addition
+    except AttributeError:
+        return None, name_str
