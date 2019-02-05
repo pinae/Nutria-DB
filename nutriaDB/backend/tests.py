dc1806da0214d749fc7616c05f5cfe9a269188cc
Pinae <jonny@pinae.net>
2019-02-05 18:01:56 +0100

    100% Test coverage for the model classes

--- nutriaDB/backend/tests.py
+++ nutriaDB/backend/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from backend.models import Product, Recipe, Ingredient, Category, Manufacturer, NoFoodException
+from backend.models import Product, Recipe, Ingredient, Category, Manufacturer, Serving
+from backend.models import NoFoodException
 
 
 class ModelTests(TestCase):
@@ -28,16 +29,6 @@ class ModelTests(TestCase):
         b.protein = 10.0
         b.save()
         self.assertAlmostEqual(r.protein, 23.0, 5)
-
-    def testIngredientNoFood(self):
-        author = User(username='foo', password='secret1234')
-        author.save()
-        cat = Category(name='test')
-        cat.save()
-        r = Recipe(category=cat, name_addition='recipe', author=author)
-        r.save()
-        i = Ingredient(recipe=r, amount=10.0)
-        self.assertRaises(NoFoodException, i.food, cat)
 
     def testPrintIngredtient(self):
         author = User(username='foo', password='secret1234')
@@ -104,6 +95,167 @@ class ModelTests(TestCase):
             i.__setattr__(prop, 10)
             self.assertAlmostEqual(i.amount, 200, 5)
 
+    def testIngredientNoFood(self):
+        def set_food(i, food):
+            i.food = food
+        author = User(username='foo', password='secret1234')
+        author.save()
+        cat = Category(name='test')
+        cat.save()
+        r = Recipe(category=cat, name_addition='recipe', author=author)
+        r.save()
+        i = Ingredient(recipe=r, amount=10.0)
+        self.assertRaises(NoFoodException, set_food, i, cat)
+
+    def testIngredientSwapProductAndRecipe(self):
+        author = User(username='foo', password='secret1234')
+        author.save()
+        cat = Category(name='test')
+        cat.save()
+        r = Recipe(category=cat, name_addition='recipe', author=author)
+        r.save()
+        a = Product(category=cat, name_addition='a', author=author, reference_amount=100.0, calories=100.0)
+        a.save()
+        i = Ingredient(recipe=r, amount=10.0)
+        i.food = a
+        i.save()
+        self.assertEqual(i.product, a)
+        self.assertIsNone(i.food_is_recipe)
+        r2 = Recipe(category=cat, name_addition='recipe2', author=author)
+        r2.save()
+        i2 = Ingredient(recipe=r, amount=10.0)
+        i2.food = a
+        i2.save()
+        i.food = r2
+        i.save()
+        self.assertIsNone(i.product)
+        self.assertEqual(i.food_is_recipe, r2)
+        i.food = a
+        i.save()
+        self.assertEqual(i.product, a)
+        self.assertIsNone(i.food_is_recipe)
+
     def testPrintManufacturer(self):
         m = Manufacturer(name="Crownfield (Lidl)")
         self.assertEqual(str(m), "Crownfield (Lidl)")
+
+    def testRecipeSetProperties(self):
+        author = User(username='foo', password='secret1234')
+        author.save()
+        cat = Category(name='test')
+        cat.save()
+        r = Recipe(category=cat, name_addition='recipe', author=author)
+        r.save()
+        a = Product(category=cat, name_addition='a', author=author, reference_amount=100.0, calories=100.0)
+        a.save()
+        b = Product(category=cat, name_addition='b', author=author, reference_amount=100.0, calories=20.0)
+        b.save()
+        i1 = Ingredient(recipe=r, amount=100.0)
+        i1.food = a
+        i1.save()
+        i2 = Ingredient(recipe=r, amount=200.0)
+        i2.food = b
+        i2.save()
+        self.assertAlmostEqual(r.calories, 140, 5)
+        r.calories = 280
+        self.assertAlmostEqual(r.calories, 280, 5)
+        self.assertAlmostEqual(r.ingredients.all()[0].amount, 200, 5)
+        self.assertAlmostEqual(r.ingredients.all()[1].amount, 400, 5)
+        r.total_fat = 1
+        r.saturated_fat = 1
+        r.cholesterol = 1
+        r.protein = 1
+        r.total_carbs = 1
+        r.sugar = 1
+        r.dietary_fiber = 1
+        r.salt = 1
+        r.sodium = 1
+        r.potassium = 1
+        r.copper = 1
+        r.iron = 1
+        r.magnesium = 1
+        r.manganese = 1
+        r.zinc = 1
+        r.phosphorous = 1
+        r.sulphur = 1
+        r.chloro = 1
+        r.fluoric = 1
+        r.vitamin_b1 = 1
+        r.vitamin_b12 = 1
+        r.vitamin_b6 = 1
+        r.vitamin_c = 1
+        r.vitamin_d = 1
+        r.vitamin_e = 1
+        props = ['total_fat', 'saturated_fat', 'cholesterol', 'protein', 'total_carbs', 'sugar', 'dietary_fiber',
+                 'salt', 'sodium', 'potassium', 'copper', 'iron', 'magnesium', 'manganese', 'zinc', 'phosphorous',
+                 'sulphur', 'chloro', 'fluoric', 'vitamin_b1', 'vitamin_b12', 'vitamin_b6', 'vitamin_c',
+                 'vitamin_d', 'vitamin_e']
+        for prop in props:
+            a.__setattr__(prop, 3)
+            b.__setattr__(prop, 10)
+        a.save()
+        b.save()
+        for prop in props:
+            self.assertAlmostEqual(r.__getattribute__(prop), 46, 5)
+        for prop in props:
+            r.__setattr__(prop, 23)
+            for prop2 in props:
+                self.assertAlmostEqual(r.__getattribute__(prop2), 23, 5)
+                self.assertAlmostEqual(r.ingredients.all()[0].amount, 100, 5)
+                self.assertAlmostEqual(r.ingredients.all()[1].amount, 200, 5)
+            r.__setattr__(prop, 46)
+            for prop2 in props:
+                self.assertAlmostEqual(r.__getattribute__(prop2), 46, 5)
+                self.assertAlmostEqual(r.ingredients.all()[0].amount, 200, 5)
+                self.assertAlmostEqual(r.ingredients.all()[1].amount, 400, 5)
+
+    def testRecipeAdjustReferenceAmount(self):
+        author = User(username='foo', password='secret1234')
+        author.save()
+        cat = Category(name='test')
+        cat.save()
+        r = Recipe(category=cat, name_addition='recipe', author=author)
+        r.save()
+        a = Product(category=cat, name_addition='a', author=author, reference_amount=100.0, calories=100.0)
+        a.save()
+        b = Product(category=cat, name_addition='b', author=author, reference_amount=100.0, calories=20.0)
+        b.save()
+        i1 = Ingredient(recipe=r, amount=100.0)
+        i1.food = a
+        i1.save()
+        i2 = Ingredient(recipe=r, amount=200.0)
+        i2.food = b
+        i2.save()
+        self.assertAlmostEqual(r.reference_amount, 300, 5)
+        r.reference_amount = 100
+        self.assertAlmostEqual(r.ingredients.all()[0].amount, 100/3, 5)
+        self.assertAlmostEqual(r.ingredients.all()[1].amount, 200/3, 5)
+        self.assertAlmostEqual(r.reference_amount, 100, 5)
+
+    def testServingFood(self):
+        def set_food(x, food):
+            x.food = food
+        author = User(username='foo', password='secret1234')
+        author.save()
+        cat = Category(name='test')
+        cat.save()
+        s = Serving(name="Testspoon", size=25)
+        self.assertRaises(NoFoodException, set_food, s, cat)
+        a = Product(category=cat, name_addition='a', author=author, reference_amount=100.0, calories=100.0)
+        a.save()
+        s.food = a
+        s.save()
+        self.assertEqual(str(s.food), "test: a")
+        self.assertIsNone(s.food_is_recipe)
+        r = Recipe(category=cat, name_addition='recipe', author=author)
+        r.save()
+        i1 = Ingredient(recipe=r, amount=100.0)
+        i1.food = a
+        i1.save()
+        s.food = r
+        self.assertEqual(str(s.food), "test: recipe")
+        self.assertIsNone(s.product)
+        s.food = a
+        s.save()
+        self.assertEqual(str(s.food), "test: a")
+        self.assertIsNone(s.food_is_recipe)
