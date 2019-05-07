23b9a80c1134b7040036f76c6a8dd96710c8769d
Pinae <jonny@pinae.net>
2019-05-07 18:40:40 +0200

    Exit if migrate fails

--- docker-setup/init_and_run.sh
+++ docker-setup/init_and_run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd /var/www
 python3 manage.py wait_for_db
-python3 manage.py migrate
+python3 manage.py migrate || { echo 'migrate failed' ; exit 1; }
 python3 manage.py collectstatic
 python3 manage.py initadmin
 python3 manage.py initial_data
