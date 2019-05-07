7ccf0d52542fa86c90a1acf46d7347cee0b95a95
Pinae <jonny@pinae.net>
2019-05-07 15:19:36 +0200

    Working directory

--- docker-setup/init_and_run.sh
+++ docker-setup/init_and_run.sh
@@ -3,4 +3,5 @@ cd /var/www
 python3 manage.py migrate
 python3 manage.py collectstatic
 python3 manage.py initadmin
+cd /home/docker
 /usr/local/bin/uwsgi --ini /etc/uwsgi/apps-enabled/uwsgi-app.ini
