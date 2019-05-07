9acff9266ab23a1b1fb917a1f084166a2fae91a4
Pinae <jonny@pinae.net>
2019-05-07 16:29:14 +0200

    Load initial data in the container

--- docker-setup/init_and_run.sh
+++ docker-setup/init_and_run.sh
@@ -3,5 +3,6 @@ cd /var/www
 python3 manage.py migrate
 python3 manage.py collectstatic
 python3 manage.py initadmin
+python3 manage.py initial_data
 cd /home/docker
 /usr/local/bin/uwsgi --ini /etc/uwsgi/apps-enabled/uwsgi-app.ini
